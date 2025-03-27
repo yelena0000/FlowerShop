5dedbbc350aaba294d2f984055f53745ff4aa5fb
Eugene Fedorov <uptiik02@gmail.com>
2025-03-27 18:55:20 +0300

    Add Buyer, Delivery and Consult models

--- flowers/admin.py
+++ flowers/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from flowers.models import Buyer, Delivery, Consult
 
-# Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('buyer_name', 'buyer_phone')
+
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ('buyer_name', 'address', 'is_finished')
+
+
+@admin.register(Consult)
+class ConsultAdmin(admin.ModelAdmin):
+    list_display = ('buyer_name', 'buyer_phone', 'is_finished')
