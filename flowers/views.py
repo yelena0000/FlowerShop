ce960daad97e271a057becc4436acdb98b7176a3
yelena0000 <kirillnek07081999@gmail.com>
2025-03-27 18:56:04 +0300

    Update views.py

--- flowers/views.py
+++ flowers/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def catalog(request):
+    return render(request, 'catalog.html')
+
+
+def flower_detail(request):
+    return render(request, 'card.html')
+
+
+def quiz(request):
+    return render(request, 'quiz.html')
+
+
+def quiz_step(request):
+    return render(request, 'quiz-step.html')
+
+
+def result(request):
+    return render(request, 'result.html')
+
+
+def order(request, pk=None):
+    return render(request, 'order.html')
+
+
+def order_step(request):
+    return render(request, 'order-step.html')
+
+
+def consultation(request):
+    return render(request, 'consultation.html')
+
+
+def signup_consultation(request):
+    return render(request, 'consultation.html')
