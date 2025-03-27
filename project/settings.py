a317ad082f094358d2d7865b526027f4e2f9ca4d
yelena0000 <kirillnek07081999@gmail.com>
2025-03-27 19:19:41 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'flowers'
+    'flowers.apps.FlowersConfig',
 ]
 
 MIDDLEWARE = [
