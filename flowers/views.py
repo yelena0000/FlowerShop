ef20a0fe6e45dd003b077f6f5af881d637a7c178
yelena0000 <kirillnek07081999@gmail.com>
2025-03-29 21:27:27 +0300

    Join frontend and backend (consultation.html)

--- flowers/views.py
+++ flowers/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from flowers.models import Buyer, Consult
+from django.contrib import messages
 
 
 def index(request):
@@ -33,6 +35,21 @@ def order_step(request):
 
 
 def consultation(request):
+    if request.method == 'POST':
+        buyer_name = request.POST.get('fname')
+        buyer_phone = request.POST.get('tel')
+
+        if not buyer_name or not buyer_phone:
+            messages.error(request, 'Пожалуйста, заполните все поля.')
+            return render(request, 'consultation.html')
+
+        buyer = Buyer.objects.create(
+            buyer_name=buyer_name,
+            buyer_phone=buyer_phone
+        )
+        Consult.objects.create(buyer_name=buyer)
+        return redirect('consultation')
+
     return render(request, 'consultation.html')
 
 
